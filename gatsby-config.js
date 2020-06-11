9a19baa5fb9e45c6769e6a07debd9e1c5f50e1d2
Adit Damodaran <aditdamodaran@gmail.com>
2020-06-11 18:34:11 -0500

    retrying

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Adit Damodaran',
+    title: 'Gatsby + Netlify CMS Starter',
     description:
-      'This is a personal portfolio I built on my own from the ground up in Gatsby.',
+      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
