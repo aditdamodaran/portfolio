d01bef46c5f0888a26f8a13b1f7f053a2754a194
Adit Damodaran <aditdamodaran@gmail.com>
2020-06-15 20:36:50 -0500

    Creating Navbar Hide on ScrollUp

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -49,8 +49,8 @@ const TemplateWrapper = ({ children }) => {
         />
       </Helmet>
       <Navbar />
-      {/*<div>{children}</div>
-      <Footer />*/}
+      <div className="main-content">{children}</div>
+      {/*<Footer />*/}
     </div>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,20 +4,86 @@ import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
 import logo from '../img/logo.svg'
 import { CSSTransition } from 'react-transition-group';
+import { transform } from 'lodash';
 
 const Navbar = class extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      isMounted: true,
       active: false,
-      navBarActiveClass: ''
+      navBarActiveClass: '',
+      scrollDirection: 'none',
+      lastScrollTop: 0,
+      menuOpen: false
     }
+    this.handleScroll = this.handleScroll.bind(this)
+  }
+
+  componentDidMount() {
+    setTimeout(
+      () =>
+        this.setState({ isMounted: true }, () => {
+          const throttle = (func, wait = 250) => {
+            let timer = null;
+            return function(...args) {
+              if (timer === null) {
+                timer = setTimeout(() => {
+                  func.apply(this, args);
+                  timer = null;
+                }, wait);
+              }
+            }
+          }
+          window.addEventListener('scroll', () => throttle(this.handleScroll()))
+        }),
+      250,
+    );
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', () => this.handleScroll())
+  }
+
+  handleScroll = () => {
+    const { isMounted, scrollDirection, lastScrollTop, menuOpen } = this.state
+    const fromTop = window.scrollY
+    const DELTA = 10
+    const navHeight = 3
+    // console.log(isMounted, scrollDirection, lastScrollTop, fromTop)
+
+    // Make sure they scroll more than DELTA (to reveal Navbar)
+    // Make sure the menu isn't open
+    // Make sure they aren't near the top (to hide Navbar) 
+    if (!isMounted 
+      || Math.abs(lastScrollTop - fromTop) <= DELTA 
+      || menuOpen
+      || fromTop < 100) {
+      return;
+    }
+
+    
+    // Determine Scroll Direction
+    if (fromTop < DELTA) {
+      this.setState({ scrollDirection: 'none' });
+    } else if (fromTop > lastScrollTop && fromTop > navHeight) {
+      if (scrollDirection !== 'down') {
+        this.setState({ scrollDirection: 'down' });
+      }
+    } else if (fromTop + window.innerHeight < document.body.scrollHeight) {
+      if (scrollDirection !== 'up') {
+        this.setState({ scrollDirection: 'up' });
+      }
+    }
+
+    this.setState({ lastScrollTop: fromTop });
   }
 
   toggleHamburger = () => {
     // toggle the active boolean in the state
     this.setState(
       {
+        menuOpen: !this.state.menuOpen,
         active: !this.state.active,
       },
       // after state has been updated,
@@ -34,12 +100,17 @@ const Navbar = class extends React.Component {
     )
   }
 
+  
   render() {
     return (
       <nav
         className="navbar"
         role="navigation"
         aria-label="main-navigation"
+        style={{
+          transition: 'transform 0.3s ease-out',
+          transform : (this.state.scrollDirection === 'down' ? `translateY(-3rem)` : `translateY(0rem)`)
+        }}
       >
         <div className="container">
 
@@ -74,7 +145,7 @@ const Navbar = class extends React.Component {
               id="navMenu"
               className={`navbar-menu ${this.state.navBarActiveClass}`}
             >
-              <div className="navbar-start has-text-centered">
+              <div className="navbar-start">
                 <Link className="navbar-item" to="/about">
                   About
                 </Link>
