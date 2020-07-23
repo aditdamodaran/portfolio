617574761c81a1a9a82b728724fda94c2561eaef
Adit Damodaran <aditdamodaran@gmail.com>
2020-07-23 18:35:29 -0500

    adjusting project spacing

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -79,7 +79,7 @@ export const IndexPageTemplate = ({
               <div className="about-me-description-segment">{aboutme.description1}</div>
               <div className="about-me-description-segment">
                 {aboutme.description2}
-                <br /><br /><a href="/about">You can read more about me here.</a>
+                <br /><br /><a href="/about">Read more...</a>
                 <br />
               </div>
             </h3>
@@ -109,12 +109,13 @@ export const IndexPageTemplate = ({
       <Work></Work>
       </div>
     </section>
-
+    
     <section id="project-previews">
+      <div className="project-previews-container">
 
       <div className="project-preview-title-container">
         <h1 className="projects-title">
-          Work Related Projects
+          Work Projects
         </h1>
       </div>
       
@@ -203,99 +204,8 @@ export const IndexPageTemplate = ({
           work={true}
         />
       </div>
-      {/*<div className="projects-title-container">
-        <h1 className="projects-title">
-          Work Related Projects
-        </h1>
-      </div>*/}
-      {/*<div className="projects-container">
-        <Project
-          title={work.projects[0].title}
-          image={work.projects[0].image}
-          text={work.projects[0].text}
-          tech={work.projects[0].tech}
-          pageLink={work.projects[0].pageLink}
-          work={true}
-        />
-        <Project
-          title={work.projects[1].title}
-          image={work.projects[1].image}
-          text={work.projects[1].text}
-          tech={work.projects[1].tech}
-          pageLink={work.projects[1].pageLink}
-          work={true}
-        />
-      </div>*/}
-
-      {/*<div className="projects-title-container">
-        <h1 className="projects-title">
-          Side Projects & Freelancing
-        </h1>
-      </div>*/}
-    
-      {/*<div className="projects-container">
-        <Project
-          title={intro.blurbs[0].title}
-          image={intro.blurbs[0].image}
-          text={intro.blurbs[0].text}
-          tech={intro.blurbs[0].tech}
-          link={intro.blurbs[0].link}
-          pageLink={intro.blurbs[0].pageLink}
-          github={intro.blurbs[0].github}
-        />
-        <Project
-          title={intro.blurbs[1].title}
-          image={intro.blurbs[1].image}
-          text={intro.blurbs[1].text}
-          tech={intro.blurbs[1].tech}
-          pageLink={intro.blurbs[1].pageLink}
-          link={intro.blurbs[1].link}
-        />
-        <Project
-          title={intro.blurbs[2].title}
-          image={intro.blurbs[2].image}
-          text={intro.blurbs[2].text}
-          pageLink={intro.blurbs[2].pageLink}
-          tech={intro.blurbs[2].tech}
-        />
-      </div>*/}
-
-      {/*<div className="projects-title-container">
-        <h1 className="projects-title">
-          Older Projects (Archived)
-        </h1>
-      </div>*/}
-
-      {/*<div className="projects-container">
-        <Project
-          title={older.projects[0].title}
-          image={older.projects[0].image}
-          text={older.projects[0].text}
-          tech={older.projects[0].tech}
-          github={older.projects[0].github}
-          pageLink={older.projects[0].pageLink}
-          work={true}
-        />
-        <Project
-          title={older.projects[1].title}
-          image={older.projects[1].image}
-          text={older.projects[1].text}
-          tech={older.projects[1].tech}
-          github={older.projects[1].github}
-          pageLink={older.projects[1].pageLink}
-          work={true}
-        />
-        <Project
-          title={older.projects[2].title}
-          image={older.projects[2].image}
-          text={older.projects[2].text}
-          tech={older.projects[2].tech}
-          github={older.projects[2].github}
-          pageLink={older.projects[2].pageLink}
-          work={true}
-        />
-      </div>*/}
-
+      
+      </div>
     </section>
     
     {/*<section id="blog">
