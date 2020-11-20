76062ff9bbd2da697a6da9f2f9034e089bb9db74
Adit Damodaran <aditdamodaran@gmail.com>
2020-11-20 13:16:37 -0800

    reverting to older Google Analytics for plugin support

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "G-BGC5ZGXN0J",
+        trackingId: "UA-80332904-3",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: true
       },
